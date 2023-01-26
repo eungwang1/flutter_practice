d2b42a56ed8efca973082750c06ed1d1f097976b
eungwang <scm323@naver.com>
2023-01-26 23:32:06 +0900

    Feat: create webtoon_home screen

---
+++ lib/screens/home_screen.webtoon.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon.model.dart';
+import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/widgets/webtoon_widget.dart';
+
+class WebtoonHomeScreen extends StatelessWidget {
+  WebtoonHomeScreen({super.key});
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 2,
+          foregroundColor: Colors.green,
+          backgroundColor: Colors.white,
+          title: const Text(
+            "오늘의 웹툰",
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+        ),
+        body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Expanded(
+                    child: makeList(snapshot),
+                  ),
+                ],
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          width: 40,
+        );
+      },
+    );
+  }
+}
